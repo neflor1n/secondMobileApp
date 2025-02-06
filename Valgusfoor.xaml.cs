259ee858d615814ddc72c71697d6665bb70b5dfb
neflor1n <opilane@HP-CZC2349HT9>
2025-02-06 14:09:14 +0200

    need to fix valgusfoor

--- Valgusfoor.xaml.cs
+++ Valgusfoor.xaml.cs
@@ -2,6 +2,8 @@
 using Microsoft.Maui.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+
 using System.Threading.Tasks;
 
 namespace secondMobileApp
@@ -10,23 +12,28 @@ namespace secondMobileApp
     {
         private bool isOn = false;
         private bool isAutoMode = false;
+        private bool isDayAndNightMode = false;
         private List<Frame> circles = new();
         private Dictionary<string, Color> colors = new()
         {
             { "punane", Colors.Red },
             { "kollane", Colors.Yellow },
             { "roheline", Colors.Green }
-        };
+        };  
 
         public Valgusfoor(int k)
         {
             InitializeComponent();
             CreateTrafficLight();
-        }
+           
+
+        } 
 
         // Создание светофора
         private void CreateTrafficLight()
-        {
+        { 
+          
+
             foreach (var key in colors.Keys)
             {
                 var frame = new Frame
@@ -47,12 +54,16 @@ namespace secondMobileApp
                     VerticalOptions = LayoutOptions.Center
                 };
 
+                
+
                 var stack = new VerticalStackLayout { Children = { frame, label } };
                 trafficLightContainer.Children.Add(stack);
+                
                 frame.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(() => ChangeText(label, key)) });
-
                 circles.Add(frame);
+
             }
+
         }
 
         // Включить светофор
@@ -72,7 +83,9 @@ namespace secondMobileApp
         private void TurnOffLight(object sender, EventArgs e)
         {
             isOn = false;
-            isAutoMode = false; 
+            isAutoMode = false;
+            isDayAndNightMode = false;
+            DayOrNight.Text = "";
             statusLabel.Text = "Valgusfoor on välja lülitatud";
             foreach (var circle in circles)
             {
@@ -146,6 +159,80 @@ namespace secondMobileApp
                 await Task.Delay(2000);
             }
 
+        }
+
+        private async void DayAndNightMode(object sender, EventArgs e)
+        {
+            if (!isOn)
+            {
+                statusLabel.Text = "Lülitage esmalt valgusfoor põlema!";
+                return;
+            }
+
+            isDayAndNightMode = true;
+
+            string[] mode = { "Päev", "Öö" };  // Переключение между режимами
+            int currentModeIndex = 0;  // Индекс текущего режима
+
+            while (isDayAndNightMode)
+            {
+                statusLabel.Text = "Päev ja öö mood aktiivne!";
+                DayOrNight.Text = mode[currentModeIndex];  // Обновление текста с режимом
+
+                // Переключение между "Päev" и "Öö" 
+                currentModeIndex = (currentModeIndex ) % mode.Length;
+
+                // Если режим "Päev"
+                if (mode[currentModeIndex] == "Päev")
+                {
+                    string[] sequence = { "punane", "kollane", "roheline" };
+                    int index = 0;
+
+
+                    for (int i = 0; i < circles.Count; i++)
+                    {
+                        circles[i].BackgroundColor = Colors.Gray;
+                    }
+
+                    circles[index].BackgroundColor = colors[sequence[index]];
+
+                    if (sequence[index] == "roheline")
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            await Task.Delay(500);
+                            circles[index].BackgroundColor = Colors.Gray; 
+                            await Task.Delay(500);
+                            circles[index].BackgroundColor = colors["roheline"]; 
+                        }
+                    }
+
+                    index = (index + 1) % sequence.Length;
+
+                    await Task.Delay(2000);
+
+                }
+                // Если режим "Öö"- мигаем только желтым
+                else if (mode[currentModeIndex] == "Öö")
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        circles[1].BackgroundColor = Colors.Gray;
+                        await Task.Delay(500);
+                        circles[1].BackgroundColor = colors["kollane"];
+                        await Task.Delay(500);
+                    }
+                }
+                await Task.Delay(5000);
+            }
+        }
+
+        private void ChangeColorToGray(object sender, EventArgs e)
+        {
+            foreach (var circle in circles)
+            {
+                circle.BackgroundColor = Colors.Gray;
+            }
         }
     }
 }
